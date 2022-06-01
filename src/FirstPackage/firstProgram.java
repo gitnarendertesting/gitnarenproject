c51ecd557dc47270c07c3872af14a01686ed2934
Rishi <Rishi@LAPTOP-TLLOPQD3>
2022-06-01 16:10:01 +0530

    first program commit

--- src/FirstPackage/firstProgram.java
+++ src/FirstPackage/firstProgram.java
@@ -1,13 +1,44 @@
 package FirstPackage;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class firstProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		
-       System.out.println("hai");
-       System.out.println("how are u");
+		/*System.setProperty("webdriver.gecko.driver","C:\\Data_other\\Selenium Jars\\geckodriver-v0.19.0-win64\\geckodriver.exe" );  
+		
+		 WebDriver driver= new FirefoxDriver(); 
+		 
+		 driver.manage().window().maximize();
+		 
+		 String homepage="https://www.amazon.in/";
+		 
+		 driver.get(homepage);
+		 
+		 Thread.sleep(3000);*/
+		 
+		
+		
+		
+		 
+		 		 }
 
 	}
 
-}
+
