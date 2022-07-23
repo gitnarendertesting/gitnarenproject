daf0bb0ef666f4dcd9c0fead2009eef0e57347ef
gitnarendertesting <105981357+gitnarendertesting@users.noreply.github.com>
2022-07-23 20:46:25 +0530

    Update testingExample.java

--- src/FirstPackage/testingExample.java
+++ src/FirstPackage/testingExample.java
@@ -4,9 +4,9 @@ public class testingExample {
 
 	public static void main(String[] args) {
 	
-		 System.out.println("Hi how are you");
-		 
-		 System.out.println("It is updated second time");
+		System.out.println("Updating test example at Master1");
+		
+		System.out.println("Updating test example at Master2");
 
 	}
 
